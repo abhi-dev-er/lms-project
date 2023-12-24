176fdac387bd8793a9d9655e4b7783a36a0e26f6
abhi-dev-er <abhishekdubeysatna55@gmail.com>
2023-12-24 13:12:12 +0530

    added checkout page

--- src/Pages/Payment/Checkout.jsx
+++ src/Pages/Payment/Checkout.jsx
@@ -3,6 +3,7 @@ import toast from "react-hot-toast";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import HomeLayout from "../../Layouts/HomeLayout";
+import { BiRupee } from "react-icons/bi";
 
 function Checkout() {
   const dispatch = useDispatch();
@@ -46,9 +47,9 @@ function Checkout() {
 
         toast.success("Payment successfull");
 
-        await dispatch(verifyUserPayment(paymentDetails));
+        const res = await dispatch(verifyUserPayment(paymentDetails));
 
-        isPaymentVerified
+        res?.payload?.success
           ? navigate("/checkout/success")
           : navigate("/checkout/fail");
       },
@@ -72,9 +73,33 @@ function Checkout() {
         className="min-h-[90vh] flex items-center justify-center text-white"
       >
         <div className="w-80 h-[26rem] flex flex-col justify-center shadow-[0_0_10px_black] rounded-lg relative">
-          <h1 className="bg-yellow-500 absolute top-0 w-full text-center py-4 text-2xl font-semibold rounded-tr-lg">
+          <h1 className="bg-yellow-500 absolute top-0 w-full text-center py-4 text-2xl font-semibold rounded-tl-lg rounded-tr-lg">
             Subscription Bundle
           </h1>
+          <div className="px-4 space-y-5 text-center">
+            <p className="text-[17px]">
+              this purchase will allow you to access all available course of our
+              platform{" "}
+              <span className="text-yellow-500 text-bold">
+                <br />1 Year duration{" "}
+              </span>
+              all the existing and new launched courses will be also available
+            </p>
+            <p className="flex items-center justify-center gap-1 text-2xl font-bold text-yellow-500">
+              <BiRupee />
+              <span>499</span> only
+            </p>
+            <div className="text-gray-200">
+              <p> 100% refund on cancellation</p>
+              <p>* Terms and conditions applied *</p>
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-800 py-2  absolute bottom-0 left-0 rounded-br-lg rounded-bl-lg font-semibold text-lg cursor-pointer hover:bg-slate-950 transition-all ease-in-out duration-300"
+            >
+              Buy Now
+            </button>
+          </div>
         </div>
       </form>
     </HomeLayout>
