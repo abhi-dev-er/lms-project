2b39d8e832493c697ee91a8f115191836006b3f3
abhi-dev-er <abhishekdubeysatna55@gmail.com>
2024-01-01 09:42:59 +0530

    adding display lecture

--- src/Pages/Dashboard/DisplayLectures.jsx
+++ src/Pages/Dashboard/DisplayLectures.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import HomeLayout from '../../Layouts/HomeLayout'
+import {  useLocation, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { getCoursesLecture } from '../../Redux/Slices/LectureSlice';
 
 function DisplayLectures() {
+
+  const nevigate = useNavigate();
+  const dispatch = useDispatch();
+  const {state} = useLocation();
+  const {lectures} = useSelector((state)=>state.lecture)
+  const {role} = useSelector((state)=>state.auth)
+
+
+  useEffect(()=> {
+ if(!state) nevigate("/courses")
+ dispatch(getCoursesLecture(state._id))
+  }, [])
   return (
-    <div>DisplayLectures</div>
+    <HomeLayout>
+      lectures
+    </HomeLayout>
   )
 }
 
