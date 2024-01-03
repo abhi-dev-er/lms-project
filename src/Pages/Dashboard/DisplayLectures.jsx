da37a192bbe99749136038acb7e6ed5005186171
abhi-dev-er <abhishekdubeysatna55@gmail.com>
2024-01-03 23:14:54 +0530

    adding display lecture ui

--- src/Pages/Dashboard/DisplayLectures.jsx
+++ src/Pages/Dashboard/DisplayLectures.jsx
@@ -9,27 +9,26 @@ function DisplayLectures() {
   const dispatch = useDispatch();
   const { state } = useLocation();
   const { lectures } = useSelector((state) => state.lecture);
-  // const {role} = useSelector((state) => state.auth);
+  const { role } = useSelector((state) => state.auth);
 
   const [currentVideo, setCurrentVideo] = useState(0);
 
   useEffect(() => {
-    // if (!state) nevigate("/courses");
-    // dispatch(getCoursesLecture(state._id));
+    if (!state) nevigate("/courses");
+    dispatch(getCoursesLecture(state._id));
   }, []);
   return (
     <HomeLayout>
       <div className="flex flex-col gap-10 items-center justify-center min-h-[90vh] py-10 text-white mx-10">
         <div className="text-center text-2xl font-semibold text-yellow-500">
-          Course Name: {state?.title} abhi
+          Course Name: {state?.title}
         </div>
 
         <div className="flex justify-center gap-10 w-full">
           {/*left section for playing videos and displaying courses details to admin*/}
           <div className="space-y-5 w-[28rem] p-2 rounded-lg shadow-[0_0_10px_black]">
             <video
-              // src={lectures && lectures[currentVideo]?.lecture?.secure_url}
-              src="https://www.pexels.com/video/silhouette-of-a-man-sitting-on-the-roof-1966695/"
+              src={lectures && lectures[currentVideo]?.lecture?.secure_url}
               className="object-fill rounded-tl-lg rounded-tr-lg w-full"
               controls
               disablePictureInPicture
@@ -38,13 +37,31 @@ function DisplayLectures() {
             ></video>
             <div>
               <h1>
-                <span className="text-yellow-500"> Title: {" "}
-                </span>
-                  Example video
-                  {/* {lectures && lectures[currentVideo]?.title} */}
+                <span className="text-yellow-500"> Title: </span>
+
+                {lectures && lectures[currentVideo]?.title}
               </h1>
+              <p>
+                <span className="text-yellow-500 line-clamp-4">
+                  Description:{" "}
+                </span>
+                {lectures && lectures[currentVideo]?.description}
+              </p>
             </div>
           </div>
+
+          {/* { right section for displaying list of lecture} */}
+
+          <ul>
+            <li>
+              <p>lectures list</p>
+              {!role === "ADMIN" && <button>Add New Lecture</button>}
+            </li>
+          </ul>
+          {lectures &&
+            lectures.map((lecture, index) => {
+              return <li key={lecture._id}></li>;
+            })}
         </div>
       </div>
     </HomeLayout>
--- src/Redux/Slices/AuthSlice.js
+++ src/Redux/Slices/AuthSlice.js
@@ -118,5 +118,5 @@ const authSlice = createSlice({
   },
 });
 
-// export const {} = authSlice.actions;
+export const {} = authSlice.actions;
 export default authSlice.reducer;
