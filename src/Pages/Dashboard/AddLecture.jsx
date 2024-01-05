fee1a1472e736ee71c881e6050abb759d07530d9
abhi-dev-er <abhishekdubeysatna55@gmail.com>
2024-01-05 12:50:27 +0530

    added add lecture functionality

--- src/Pages/Dashboard/AddLecture.jsx
+++ src/Pages/Dashboard/AddLecture.jsx
@@ -1,36 +1,58 @@
-import React, { useState } from 'react'
-import HomeLayout from '../../Layouts/HomeLayout'
-import { useLocation, useNavigate } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
+import React, { useEffect, useState } from "react";
+import HomeLayout from "../../Layouts/HomeLayout";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import toast from "react-hot-toast";
+import { addCoursesLecture } from "../../Redux/Slices/LectureSlice";
 
 function AddLecture() {
-    const courseDetails = useLocation().state
+  const courseDetails = useLocation().state;
 
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const [userInput, setUserInput] = useState({
+  const [userInput, setUserInput] = useState({
+    id: courseDetails._id,
+    lecture: undefined,
+    title: "",
+    description: "",
+    videoSrc: "",
+  });
+
+  function handleInputChange(e) {
+    const video = e.target.files[0];
+    const source = window.URL.createObjectURL(video);
+    setUserInput({
+      ...userInput,
+      lecture: video,
+      videoSrc: source,
+    });
+  }
+
+  async function onFormSubmit(e) {
+    e.preventDefault();
+    if (!userInput.lecture || !userInput.title || !userInput.description) {
+      toast.error("All fields are mandatory");
+      return;
+    }
+
+    const response = await dispatch(addCoursesLecture(userInput));
+
+    if (response?.payload?.success) {
+      setUserInput({
         id: courseDetails._id,
         lecture: undefined,
         title: "",
         description: "",
         videoSrc: "",
-    });
-
-    function handleInputChange(e){
-        const video = e.target.files[0];
-        const source = window.URL.createObjectURL(video);
-        setUserInput({
-            ...userInput,
-            lecture: video,
-            videoSrc: source
-        })
+      });
     }
-  return (
-    <HomeLayout>
+  }
 
-    </HomeLayout>
-  )
+  useEffect(() =>{
+    if(!courseDetails) navigate("/courses");
+  }, [])
+  return <HomeLayout></HomeLayout>;
 }
 
-export default AddLecture
+export default AddLecture;
