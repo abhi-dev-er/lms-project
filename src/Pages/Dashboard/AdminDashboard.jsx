4de2702e71a56a68263ee1a54464dbafc1b1df79
abhi-dev-er <abhishekdubeysatna55@gmail.com>
2024-01-07 13:25:42 +0530

    added admin dashboard

--- src/Pages/Dashboard/AdminDashboard.jsx
+++ src/Pages/Dashboard/AdminDashboard.jsx
@@ -12,6 +12,9 @@ import React, { useEffect } from "react";
 import HomeLayout from "../../Layouts/HomeLayout";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import getAllCourses from "../../Redux/Slices/CourseSlice"
+import getStatsData from "../../Redux/Slices/StatSlice"
+import getPaymentRecord from "../../Redux/Slices/RazorpaySlice"
 
 ChartJS.register(
   ArcElement,
@@ -32,6 +35,18 @@ function AdminDashboard() {
     (state) => state.razorpay
   );
 
+  const userData ={
+    levels: ["Registerd User", "Enrolled User" ],
+    datasets: [
+      {
+        label: "User Details",
+        data: [allUserCount, subscribedCount],
+        backgroundColor: ["yellow", "green"],
+        borderWidth: 1,
+        borderColor: ["yellow", "green"]
+      }
+    ]
+  }
   useEffect(()=>{
     (
         async () => {
