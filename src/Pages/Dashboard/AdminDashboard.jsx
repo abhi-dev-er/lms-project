5050971c0c735ecccf3111a35c757b2135988bf8
abhi-dev-er <abhishekdubeysatna55@gmail.com>
2024-01-08 15:24:22 +0530

    adding ui for admin dashboard

--- src/Pages/Dashboard/AdminDashboard.jsx
+++ src/Pages/Dashboard/AdminDashboard.jsx
@@ -9,7 +9,9 @@ import {
   Title,
 } from "chart.js";
 import React, { useEffect } from "react";
-import {FaUsers} from "react-icons/fa"
+import { FaUsers } from "react-icons/fa";
+import { FcSalesPerformance } from "react-icons/fc";
+import { GiMoneyStack } from "react-icons/gi";
 import HomeLayout from "../../Layouts/HomeLayout";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
@@ -17,7 +19,7 @@ import getAllCourses from "../../Redux/Slices/CourseSlice";
 import getStatsData from "../../Redux/Slices/StatSlice";
 import getPaymentRecord from "../../Redux/Slices/RazorpaySlice";
 import deleteCourses from "../../Redux/Slices/CourseSlice";
-import { Pie } from "react-chartjs-2";
+import { Bar, Pie } from "react-chartjs-2";
 
 ChartJS.register(
   ArcElement,
@@ -115,7 +117,7 @@ function AdminDashboard() {
                   <p className="font-semibold">Registered Users</p>
                   <h3 className="text-4xl font-bold">{allUserCount}</h3>
                 </div>
-                <FaUsers className="text-yellow-500 text-5xl"/>
+                <FaUsers className="text-yellow-500 text-5xl" />
               </div>
 
               <div className="flex items-center justify-between p-5 gap-5 rounded-md shadow-md">
@@ -123,13 +125,51 @@ function AdminDashboard() {
                   <p className="font-semibold">Subscribed</p>
                   <h3 className="text-4xl font-bold">{subscribedCount}</h3>
                 </div>
-                <FaUsers className="text-green-500 text-5xl"/>
+                <FaUsers className="text-green-500 text-5xl" />
               </div>
             </div>
           </div>
 
-          <div className="flex flex-col items-center gap-10 p-5 shadow-lg shadow-md">
+          <div className="flex flex-col items-center gap-10 p-5 shadow-lg">
+            <div className="h-80 w-full relative">
+              <Bar className="absolute bottom-0 h-80 w-full" data={salseData} />
+            </div>
 
+            <div className="grid grid-cols-2 gap-5">
+              <div className="flex items-center justify-between p-5 gap-5 rounded-md shadow-md">
+                <div className="flex flex-col items-center">
+                  <p className="font-semibold">Subscription count</p>
+                  <h3 className="text-4xl font-bold">{allPayments?.count}</h3>
+                </div>
+                <FcSalesPerformance className="text-yellow-500 text-5xl" />
+              </div>
+
+              <div className="flex items-center justify-between p-5 gap-5 rounded-md shadow-md">
+                <div className="flex flex-col items-center">
+                  <p className="font-semibold">Total revenue</p>
+                  <h3 className="text-4xl font-bold">
+                    {allPayments?.count * 499}
+                  </h3>
+                </div>
+                <GiMoneyStack className="text-yellow-500 text-5xl" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mx-[10%] w-[80%] self-center flex flex-col items-center justify-center gap-10 mb-10">
+          <div className="flex w-full items-center justify-between">
+            <h1 className="text-center text-3xl font-semibold">
+              Courses Overview
+            </h1>
+            <button
+              onClick={() => {
+                navigate("/course/create");
+              }}
+              className="w-fit bg-yellow-500 hover:bg-yellow-600 transition-all ease-in-out duration-300 rounded py-2 px-4 font-semibold text-lg cursor-pointer"
+            >
+              Create New Course
+            </button>
           </div>
         </div>
       </div>
